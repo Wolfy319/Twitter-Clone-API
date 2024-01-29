7ac6a168eb8f6e5dd0bc5f59a6a044498a31cb58
Wolfy319 <jonwolffiorini@gmail.com>
2024-01-29 13:09:43 -0800

    started user infrastructure

---
+++ src/main/java/com/cooksys/socialmedia/services/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.cooksys.socialmedia.services.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.cooksys.socialmedia.services.UserService;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService {
+
+}
