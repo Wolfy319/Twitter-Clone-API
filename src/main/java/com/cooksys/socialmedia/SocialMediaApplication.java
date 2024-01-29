b1d2ca60dcb71f213f18aa5a2b40e9d786a9e704
Nick Miller <22.nickmiller@gmail.com>
2024-01-29 14:03:24 -0500

    - Updated GroupID in pom.xml - Refactored packages

--- src/main/java/com/cooksys/socialmedia/SocialMediaApplication.java
+++ src/main/java/com/cooksys/socialmedia/SocialMediaApplication.java
@@ -1,4 +1,4 @@
-package com.example.socialmedia;
+package com.cooksys.socialmedia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/cooksys/socialmedia/SocialMediaApplicationTests.java
+++ src/test/java/com/cooksys/socialmedia/SocialMediaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.socialmedia;
+package com.cooksys.socialmedia;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
