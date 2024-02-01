722fd30ae754dd41059305a81b7714d4b95dc0d8
Nick Miller <22.nickmiller@gmail.com>
2024-02-01 15:21:48 -0500

    Updated Hashtag endpoint logic

--- src/main/java/com/cooksys/socialmedia/controllers/HashtagController.java
+++ src/main/java/com/cooksys/socialmedia/controllers/HashtagController.java
@@ -1,10 +1,13 @@
 package com.cooksys.socialmedia.controllers;
 
 import com.cooksys.socialmedia.dtos.HashtagDto;
+import com.cooksys.socialmedia.dtos.TweetResponseDto;
+import com.cooksys.socialmedia.exceptions.NotFoundException;
 import com.cooksys.socialmedia.services.HashtagService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,18 +35,21 @@ public class HashtagController {
         return ResponseEntity.ok(hashtags);
     }
 
-    //TODO: Uncomment and test when Tweets are implemented
-//    /**
-//     * Retrieves all (non-deleted) tweets tagged with the given hashtag label.
-//     * The tweets should appear in reverse-chronological order.
-//     * If no hashtag with the given label exists, an error should be sent in lieu of a response.
-//     *
-//     * @param label the label of the hashtag for which to retrieve tweets
-//     * @return a ResponseEntity containing a list of tweets (TweetResponseDto) tagged with the hashtag
-//     */
-//    @GetMapping("/{label}")
-//    public ResponseEntity<List<TweetResponseDto>> getTweetsByHashtag(@PathVariable String label) {
-//        List<TweetResponseDto> tweets = hashtagService.getTweetsByHashtag(label);
-//        return tweets.isEmpty() ? ResponseEntity.notFound().build() : ResponseEntity.ok(tweets);
-//    }
+    /**
+     * Retrieves all (non-deleted) tweets tagged with the given hashtag label.
+     * The tweets should appear in reverse-chronological order.
+     * If no hashtag with the given label exists, an error should be sent in lieu of a response.
+     *
+     * @param label the label of the hashtag for which to retrieve tweets
+     * @return a ResponseEntity containing a list of tweets (TweetResponseDto) tagged with the hashtag
+     */
+    @GetMapping("/{label}")
+    public ResponseEntity<List<TweetResponseDto>> getTweetsByHashtag(@PathVariable String label) {
+        List<TweetResponseDto> tweets = hashtagService.getTweetsByHashtag(label)
+                .orElseThrow(() -> new NotFoundException(
+                "Hashtag not found with label: '" + label + "'. Hint: Use '%23' for '#' in hashtags, e.g., /tags/%23mario."
+        ));
+
+        return ResponseEntity.ok(tweets);
+    }
 }
--- src/main/java/com/cooksys/socialmedia/repositories/TweetRepository.java
+++ src/main/java/com/cooksys/socialmedia/repositories/TweetRepository.java
@@ -1,10 +1,14 @@
 package com.cooksys.socialmedia.repositories;
 
+import com.cooksys.socialmedia.entities.Hashtag;
 import com.cooksys.socialmedia.entities.Tweet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
+    List<Tweet> findByHashtagsAndDeletedFalseOrderByPostedDesc(Hashtag hashtag);
 
 }
